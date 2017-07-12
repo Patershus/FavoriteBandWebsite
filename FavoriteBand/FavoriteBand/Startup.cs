dacd3f7b15a28523cab79abbbbeac406ce32f3c0
Petter Olgemar <petterolgemar_92@hotmail.com>
2017-07-12 11:30:02 +0200

    Tog bort skräp

--- FavoriteBand/FavoriteBand/Startup.cs
+++ FavoriteBand/FavoriteBand/Startup.cs
@@ -22,11 +22,9 @@ namespace FavoriteBand
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-
             
-                app.UseDeveloperExceptionPage();
+            app.UseDeveloperExceptionPage();
             
-
             app.UseMvcWithDefaultRoute();
         }
     }
